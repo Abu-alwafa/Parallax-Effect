86cfefeccb2204fc65ce1eaab9424e0731a739e1
Abu-alwafa <abu.alwafa892@gmail.com>
2022-02-01 12:22:01 +0300

    remove parallax effect from small screens

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,34 @@
-let navLinks = document.querySelectorAll('.navbar li a');
-let moon = document.querySelector('.moon');
-let stars = document.querySelector('.stars');
-let mountains1 = document.querySelector('.mountains1');
-let river = document.querySelector('.river');
-let mountains2 = document.querySelector('.mountains2');
-let boat = document.querySelector('.boat');
-navLinks.forEach(link => {
-  link.addEventListener('click', function () {
-    navLinks.forEach(link => link.classList.remove('active'));
-    this.classList.add('active');
-  })
-})
+let navLinks = document.querySelectorAll(".navbar li a");
+let moon = document.querySelector(".moon");
+let stars = document.querySelector(".stars");
+let mountains1 = document.querySelector(".mountains1");
+let river = document.querySelector(".river");
+let mountains2 = document.querySelector(".mountains2");
+let boat = document.querySelector(".boat");
+navLinks.forEach((link) => {
+  link.addEventListener("click", function () {
+    navLinks.forEach((link) => link.classList.remove("active"));
+    this.classList.add("active");
+  });
+});
 
 onscroll = () => {
-  moon.style.top = scrollY * 1.5 + "px";
-  stars.style.left = scrollY * 0.3 + "px";
-  mountains1.style.top = scrollY * 0.5 + "px";
-  mountains2.style.top = scrollY * 0.2 + "px";
-  river.style.top = scrollY * 0.2 + "px";
-  boat.style.top = scrollY * 0.2 + "px";
-  boat.style.left = scrollY * 1.2  + "px";
- 
-  if (scrollY >= 300) {
-    moon.style.display = "none";
-    document.body.style.background = "linear-gradient(#5ac5d3, black)"
+  if (screen.width > 768) {
+    moon.style.top = scrollY * 1.5 + "px";
+    stars.style.left = scrollY * 0.3 + "px";
+    mountains1.style.top = scrollY * 0.5 + "px";
+    mountains2.style.top = scrollY * 0.2 + "px";
+    river.style.top = scrollY * 0.2 + "px";
+    boat.style.top = scrollY * 0.2 + "px";
+    boat.style.left = scrollY * 1.2 + "px";
+
+    if (scrollY >= 300) {
+      moon.style.display = "none";
+      document.body.style.background = "linear-gradient(#5ac5d3, black)";
+    } else {
+      moon.style.display = "block";
+      document.body.style.background =
+        "linear-gradient(rgb(122, 1, 122), black)";
+    }
   }
-  else {
-    moon.style.display = "block"
-    document.body.style.background = "linear-gradient(rgb(122, 1, 122), black)"
-  };
-}
+};
